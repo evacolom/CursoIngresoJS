b1bd12e44dab7e9a484a6ada1f46e3afa2c14b2d
evacolom <maricel8532@gmail.com>
2022-02-03 19:54:49 -0300

    Ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,16 @@
 /*
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
-function mostrar()
-{	
-	alert("ok");
-}
 
+
+function mostrar() {
+let nombre;
+
+     nombre = document.getElementById("txtIdNombre").value;
+ 
+let edad;
+	 edad = document.getElementById("txtIdEdad").value;
+
+nombreyedad=(" Usted se llama ") + nombre + (" y tiene " ) + edad + (" años");
+alert(nombreyedad);
+}
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -3,7 +3,8 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
-	alert("ok");
-}
 
+function sumar {
+
+document.getElementById("txtIdNumeroUno")
+document.getElementById("txtIdNumeroDos")
