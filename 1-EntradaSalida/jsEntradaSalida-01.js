8b504f3e5e3a0ac14c9fa5bc1482db5c1f2e2b92
evacolom <maricel8532@gmail.com>
2022-02-03 16:28:08 -0300

    Ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
-/*
+/*Comentarios
+Eva Colom
+Ejercicio 1
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("¡Hola! Esto funciona de once");
 }
 
