c86198d875b30861ce802b6a3a60c12e1336028a
evacolom <maricel8532@gmail.com>
2022-02-04 17:27:39 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,71 @@
 /*
-Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
-mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
+Eva Colom
+Ejercicio 7*/ 
+
 function sumar()
 {	
-	alert("ok");	
+	let numero1;
+	let numero2;
+	let mensajeAMostrar;
+
+numero1 = document.getElementById("txtIdNumeroUno").value;
+numero2 = document.getElementById("txtIdNumeroDos").value;
+
+numeroparciado1 = parseInt(numero1);
+numeroparciado2 = parseInt(numero2);
+
+ mensajeAMostrar = "La suma es " + (numeroparciado1 + numeroparciado2);
+ alert(mensajeAMostrar);
 }
 
 function restar()
 {
+	let numero1;
+	let numero2;
+	let mensajeAMostrar;
+
+numero1 = document.getElementById("txtIdNumeroUno").value;
+numero2 = document.getElementById("txtIdNumeroDos").value;
+
+numeroparciado1 = parseInt(numero1);
+numeroparciado2 = parseInt(numero2);
+
+ mensajeAMostrar = "La resta es " + (numeroparciado1 - numeroparciado2);
+ alert(mensajeAMostrar);
 	
 }
 
 function multiplicar()
 { 
-	
+	let numero1;
+	let numero2;
+	let mensajeAMostrar;
+
+numero1 = document.getElementById("txtIdNumeroUno").value;
+numero2 = document.getElementById("txtIdNumeroDos").value;
+ 
+numeroparciado1 = parseInt(numero1);
+numeroparciado2 = parseInt(numero2);
+
+ mensajeAMostrar = "La multiplicación es " + (numeroparciado1 * numeroparciado2);
+ alert(mensajeAMostrar);
 }
 
 function dividir()
 {
 	
+	let numero1;
+	let numero2;
+	let mensajeAMostrar;
+
+numero1 = document.getElementById("txtIdNumeroUno").value;
+numero2 = document.getElementById("txtIdNumeroDos").value;
+
+numeroparciado1 = parseInt(numero1);
+numeroparciado2 = parseInt(numero2);
+
+ mensajeAMostrar = "La división es " + (numeroparciado1 / numeroparciado2);
+ alert(mensajeAMostrar);
+
 }
 
