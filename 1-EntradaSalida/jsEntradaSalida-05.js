370d80033956849c9a71c711610c99b2bf6bdac7
evacolom <maricel8532@gmail.com>
2022-02-10 01:10:05 -0300

    Ejercicio 5 bis

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,6 +1,9 @@
 /*
+Eva Colom 
+Ejercicio 5 bis.
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+ej.: "Usted se llama José y tiene 66 años" 
+bis: Se debe mostrar el mensaje: "Perez, usted se llama José y tiene 66 años" se debe obtener el apellido, de la manera que puedan 	*/
 
 
 function mostrar() {
@@ -11,6 +14,12 @@ let nombre;
 let edad;
 	 edad = document.getElementById("txtIdEdad").value;
 
-nombreyedad=(" Usted se llama ") + nombre + (" y tiene " ) + edad + (" años");
+
+let apellido;
+     apellido = prompt("Ingrese su apellido");
+
+
+
+nombreyedad= apellido + (", usted se llama ") + nombre + (" y tiene " ) + edad + (" años");
 alert(nombreyedad);
 }
