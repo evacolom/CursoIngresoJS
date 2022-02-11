0d9f47cbab597e5f9a3d8c7240a6e581efeeddcc
evacolom <maricel8532@gmail.com>
2022-02-11 16:54:27 -0300

    Ejercicio 4 Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,29 @@
+
+/* Eva Colom
+   Ejercicio 4 Switch */
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+
+	let mes = document.getElementById("txtIdMes").value;
+
+	switch(mes){
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 días");
+		  break;
+		case "Febrero":
+			alert("Este mes tiene 28 días");
+		  break;
+		default:
+			alert("Este mes tiene 31 días");
+		  break;
+		  
+
+		
+	}
 
 
-
-}//FIN DE LA FUNCIÓN
+}
