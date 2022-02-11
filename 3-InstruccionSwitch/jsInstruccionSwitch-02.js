8e8fb14009b3c48983abe194d39c2f65931cc0dc
evacolom <maricel8532@gmail.com>
2022-02-11 16:40:10 -0300

    Ejercicio 2 y 3 Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -2,6 +2,9 @@
 Ejercicio 2 Switch */
 
 
+/* Eva Colom
+ Ejercicio 2 Switch*/
+
 function mostrar()
 {
 	let mes = document.getElementById("txtIdMes").value;
@@ -9,17 +12,19 @@ function mostrar()
 	 switch(mes){
 	   case "Julio":
 	   case "Agosto":
-	   alert("Abrigate que hace frío");
-	   break;
+		   alert("Abrigate que hace frío");
+	       break;
 	   case "Septiembre":
 	   case  "Octubre":
 	   case "Noviembre": 
 	   case "Diciembre":
-	   alert("Ya pasó el frío, ahora calor!!");
-	   break;
-	   default : alert("Falta para el invierno");
+		   alert("Ya pasó el frío, ahora calor!!");
+	       break;
+	   default : 
+	       alert("Falta para el invierno");
+	       break;
 
-	}
+	    }
 
 }
 
--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,19 @@
+/* Ejercicio 3
+Eva Colom */
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
+	let mes = document.getElementById("txtIdMes").value;
+
+	switch(mes) {
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días");
+		break;
+	default: 
+	        alert("Este mes tiene 30 días o más");
+		break;
+	}
 	
 
 
-}//FIN DE LA FUNCIÓN
+}
