2a2438eddb2d562c481024fd69f657189a66a3a5
evacolom <maricel8532@gmail.com>
2022-02-11 16:09:50 -0300

    Ejercicio 2 Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,26 @@
+/* Eva Colom
+Ejercicio 2 Switch */
+
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value;
+
+	 switch(mes){
+	   case "Julio":
+	   case "Agosto":
+	   alert("Abrigate que hace frío");
+	   break;
+	   case "Septiembre":
+	   case  "Octubre":
+	   case "Noviembre": 
+	   case "Diciembre":
+	   alert("Falta para el invierno");
+	   break;
+	   default : alert("Ya pasamos el frio, ahora calor!!!");
+
+	}
+
+}
 
 
-
-
-}//FIN DE LA FUNCIÓN
