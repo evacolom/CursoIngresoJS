a6991c183a61cfe1a9cf24724fb9a1727c4469b8
evacolom <maricel8532@gmail.com>
2022-02-11 14:37:16 -0300

    TP 3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,37 @@
-/*3.	Para el departamento de Pinturas:
+/*
+Eva Colom
+Trabajo Práctico 3
+
+3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
+
+    let fahrenheit;
+    let centigrados;
+    let mensajeAMostrar;
+
+    fahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+    
+    centigrados = (fahrenheit - 32)  * 5 / 9;
+
+    mensajeAMostrar = fahrenheit + " grados fahrenheit " + " equivalen a " + centigrados + " grados centigrados";
 	
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let centigrados;
+    let fahrenheit;
+    let mensajeAMostrar;
+
+    centigrados = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    fahrenheit = 9 * centigrados / 5 + 32;
+
+    mensajeAMostrar = centigrados + " grados centigrados " + " equivalen a " + fahrenheit +  " grados fahrenheit ";
+
+    
 }
