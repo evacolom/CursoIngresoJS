3ce36f73185940f2ef5374f8ec1be1f076bfdf3a
evacolom <maricel8532@gmail.com>
2022-02-18 15:36:48 -0300

    Ej 7 while

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,18 +1,27 @@
 /*
+Eva Colom
+Ejercicio 7 WHILE
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+	let contador=0;
+	let acum=0;
+	let respuesta="s";
+	let promedio;
+	let numeroIngresado;
 
+	while(respuesta == "s"){
+		numero = parseFloat(prompt("Ingrese un número"));
+		acum = acum + numeroIngresado;
+		contador  = contador + 1;
+		respuesta = "¿Continuar? s/n";
+	}
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+	promedio = acum  / contador;
+	
+	documet.getElementById("txtIdSuma").value = acum ;
+	document.getElementById("txtIdPromedio").value= promedio;
 
-}//FIN DE LA FUNCIÓN
+}
